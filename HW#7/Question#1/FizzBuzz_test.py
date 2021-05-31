2cd7c11a1ff6eeee435785dfb0479805a25f5f6b
Michael <smitmic5@oregonstate.edu>
2021-05-31 13:12:33 -0700

    wrote second test for finding all multiples of 5/all buzzs

--- HW#7/Question#1/FizzBuzz_test.py
+++ HW#7/Question#1/FizzBuzz_test.py
@@ -9,3 +9,11 @@ class test(unittest.TestCase):
             if i == "fizz":
                 x += 1
         self.assertEqual(x,27)
+
+    def test_divByFive(self):
+        result = FizzBuzz.Fizz_Buzz()
+        x = 0
+        for i in result:
+            if i == "buzz":
+                x += 1
+        self.assertEqual(x,14)
