6628c45cf88a156c500057f019c49ba41e3f1495
Michael <smitmic5@oregonstate.edu>
2021-05-31 13:37:25 -0700

    wrote second test for an exceptable leap year input

--- HW#7/Question#2/leapYear_test.py
+++ HW#7/Question#2/leapYear_test.py
@@ -5,3 +5,7 @@ class test(unittest.TestCase):
     def test_inputError(self):
         result = LeapYear.leap_year("test")
         self.assertEqual(result, "Invalid input")
+
+    def test_leapYear(self):
+        result = LeapYear.leap_year("2000")
+        self.assertEqual(result, "2000 is a leap year")
