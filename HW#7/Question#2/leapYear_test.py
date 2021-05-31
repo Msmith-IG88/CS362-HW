aa3c5e3760d01632234cf26deb901ab349489e43
Michael <smitmic5@oregonstate.edu>
2021-05-31 13:40:48 -0700

    wrote third test for when an input is not a leap year

--- HW#7/Question#2/leapYear_test.py
+++ HW#7/Question#2/leapYear_test.py
@@ -9,3 +9,7 @@ class test(unittest.TestCase):
     def test_leapYear(self):
         result = LeapYear.leap_year("2000")
         self.assertEqual(result, "2000 is a leap year")
+
+    def test_notLeapYear(self):
+        result = LeapYear.leap_year("2001")
+        self.assertEqual(result, "2001 is not a leap year")
