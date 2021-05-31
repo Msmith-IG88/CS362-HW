2a9a531846aaef871d8ad5fe85c27434936a7aa8
Michael <smitmic5@oregonstate.edu>
2021-05-31 13:39:14 -0700

    implemented calculations for a leap year and correctly presenting it to user

--- HW#7/Question#2/LeapYear.py
+++ HW#7/Question#2/LeapYear.py
@@ -1,3 +1,8 @@
 def leap_year(year):
     if (year.isdigit() == False):
         return "Invalid input"
+    else:
+        if ( int(year) % 4 == 0 and int(year) % 100 != 0 or int(year) % 400 == 0):
+            return year + " is a leap year"
+
+print(leap_year("2000"))
