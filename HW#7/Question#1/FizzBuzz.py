91c0d55611d934d9e8289cb936c52eb077fb31a2
Michael <smitmic5@oregonstate.edu>
2021-05-31 13:09:23 -0700

    implemented multiples of three method to create a list of all numbers between 1 and 100 while adding fizz's

--- HW#7/Question#1/FizzBuzz.py
+++ HW#7/Question#1/FizzBuzz.py
@@ -1,2 +1,15 @@
 def Fizz_Buzz():
-    pass
+    fizzbuzz = []
+    for i in range(101):
+        if i == 0:
+            continue
+        elif i % 3 == 0 and i % 5 == 0:
+            fizzbuzz.append(i)
+            continue
+        elif i % 3 == 0:
+            fizzbuzz.append("fizz")
+            continue
+        fizzbuzz.append(i)
+    return fizzbuzz
+
+print(Fizz_Buzz())
